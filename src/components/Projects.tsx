2fbac2900dc3de302b80743e21a01ee5e658c1c2
203407 <203407@ids.upchiapas.edu.mx>
2024-04-10 22:06:24 -0600

    feat: recomendation

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -12,8 +12,18 @@ function Projects({proyectos} :ProyectProps) {
 
         <div className="container " id="projects">
             <h1 className="All__title">Proyectos</h1>
+
+            <div className="reco__container">
+                <h3 className="reco__tittle">Recomendaciones: </h3>
+                <p className="reco__parrafo">
+                    El backend está en un servidor gratuito, así que se pausa después de cierto tiempo de inactividad, si cuando ingresa a alguno de los proyectos no reacciona ante las peticiones, por favor de esperar un minuto o dos tras la primera petición para que el servidor encienda y reacciones, gracias. 
+                </p>
+            </div>
+            
+
+
                         
-            <div className="proyectos__all__container">
+            <div className="proyectos__all__container"> 
             
             {
                 proyectos.map(pr => <Pro key={pr.name} name={pr.name} descripcion={pr.descripcion} img={pr.img} tecnologias={pr.tecnologias} url={pr.url} front={pr.front} back={pr.back}/>)
